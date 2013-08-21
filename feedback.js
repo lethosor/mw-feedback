74525e0e914d4df193aab360fe1e1689bf4a2ba4
lethosor <stahlalan19@gmail.com>
2013-08-21 17:12:56 -0400

    Add form

--- feedback.js
+++ feedback.js
@@ -2,11 +2,14 @@
  * MediaWiki article feedback tool
  */
 jQuery(function($){
-	var F = {ui:{}};
+	var F = {ui:{}, page:{}};
 	var PD=function(e){ // preventDefault shorthand
 		if(e && 'preventDefault' in e && 'call' in e.preventDefault)
 			e.preventDefault()
 	};
+	
+	F.page.name = wgPageName.replace(/_/g, ' ');
+	
 	F.opts = {
 		'watch': true,
 		'triggers': [['section', 'Feedback']]
@@ -55,7 +58,22 @@ jQuery(function($){
 	F.exit = function(e){PD(e);
 		F.ui.hide();
 		return true;
-	}
+	};
+	
+	F.save = function(e){PD(e);
+		title = F.ui.f_title.val() || F.ui.f_title.attr('placeholder');
+		text = F.ui.f_text.val();
+		if (!text) {
+			return F.save_error('Feedback is required!');
+		}
+		
+		return true;
+	};
+	
+	F.save_error = function(err){
+		F.ui.error.text(err).fadeIn(200);
+		return false;
+	};
 	
 	F.ui.show = function(e){PD(e);
 		F.ui.overlay.fadeIn(300);
@@ -63,8 +81,10 @@ jQuery(function($){
 	};
 	
 	F.ui.hide = function(e){PD(e);
-		F.ui.overlay.fadeOut(300);
-		F.ui.screen.fadeOut(300);
+		F.ui.overlay
+		.add(F.ui.screen)
+		.add(F.ui.error)
+		.fadeOut(300);
 	};
 	
 	F.ui.overlay = $('<div>').css({
@@ -86,6 +106,45 @@ jQuery(function($){
 	
 	F.ui.screen.append('<h3>Feedback</h3>');
 	
+	F.ui.f_title = $('<input>').attr({type:'text', placeholder:'Feedback for '+F.page.name})
+	.appendTo(F.ui.screen).css({
+		'font-size': '1.2em'
+	});
+	F.ui.f_text = $('<textarea>').appendTo(F.ui.screen).css({
+		height:'50%',
+		'font-size': '1em'
+	}).attr({placeholder:'Feedback'});
+	F.ui.f_text.add(F.ui.f_title).css({
+		width: '95%',
+		border: '1px solid #ccc',
+		'border-radius': 2,
+		color: '#333',
+		margin: 6,
+		'padding': 5
+	});
+	F.ui.watch = $('<input>').attr({type:'checkbox', id:'F-watch'}).appendTo(F.ui.screen)
+	.css({
+		'float':'left'
+	}).prop('checked', F.opts.watch);
+	F.ui.watch_label = $('<label for="F-watch">').text('Watch for changes')
+	.insertAfter(F.ui.watch);
+	if (!wgUserName) {
+		F.ui.watch_label.hide();
+		F.opts.watch = false;
+	}
+	F.ui.b_submit = $('<button>').text('Submit feedback').appendTo(F.ui.screen).click(F.save);
+	F.ui.b_cancel = $('<button>').text('Cancel').appendTo(F.ui.screen).click(F.exit);
+	F.ui.b_cancel.add(F.ui.b_submit).css({
+		color: '#333',
+		'background-color': '#ddd',
+		border: 'none',
+		'border-radius': 2,
+		'float': 'right',
+		'margin-right': 5,
+		cursor: 'pointer',
+		'font-size': '1em'
+	});
+	F.ui.error = $('<div>').css({color:'red', 'clear':'both'}).appendTo(F.ui.screen);
 	
 	F.setup();
 	window.Feedback = F;
